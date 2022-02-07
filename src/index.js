9f4eb60ec6713dbfd176e702059a91456f5bed48
Connie Skomra <connie@cskomra.com>
2022-02-07 08:03:36 -0500

    add redux, implement userReducer and userActions

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import store from './redux/store';
+
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
+  <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
         <App />
       </React.StrictMode>
-    </BrowserRouter>,
-    document.getElementById('root')
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
